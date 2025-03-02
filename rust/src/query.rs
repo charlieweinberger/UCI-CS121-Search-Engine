f1fac4c97bf51edf1984d004df1538b5b26123d4
Priyansh Shah <Priyanshpokemon@gmail.com>
2025-03-01 20:04:17 -0800

    multithreading the querying as well

--- rust/src/query.rs
+++ rust/src/query.rs
@@ -7,6 +7,8 @@ use std::{
 use crate::id_book::IDBookElement;
 use crate::{file_skip_list, tokenizer::Tokenizer};
 use std::fs::File;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 pub struct SearchEngine {
     query: String,
@@ -36,31 +38,52 @@ impl SearchEngine {
         let time = time::Instant::now();
         println!("Searching for: \"{}\"", self.query);
         println!("Tokens: {:?}", self.tokens);
-        let mut candidates = Vec::with_capacity(self.tokens.len());
+        // this will be shared across threads for adding candidates
+        let candidates = Arc::new(Mutex::new(Vec::with_capacity(self.tokens.len())));
+        // create a thread for each token to search for the token in the inverted index
+        let mut handles = vec![];
+
         for (index, token) in self.tokens.iter().enumerate() {
-            // get the first letter of the token to determine which file to read
-            if let Some(first_char) = token.chars().next() {
-                // go to the file_skip list of the first character
-                let skiplist = file_skip_list::FileSkip::read_skip_list(first_char);
-                // get which BYTE range it is in between
-                let offset_range =
-                    file_skip_list::FileSkip::find_skip_entry(&skiplist, token.to_string());
-                let file_path = format!("inverted_index/merged/{}.txt", first_char);
-                candidates.push(Candidate::new(token.to_string()));
-                if let Ok(file) = File::open(&file_path) {
-                    // get the postings from the file and update the scorings of the candidates
-                    let postings =
-                        file_skip_list::get_postings_from_offset_range(&file, offset_range, token);
-                    let candidate = candidates.get_mut(index).unwrap();
-                    // Update candidates with the postings data
-                    for single_posting in postings.postings {
-                        candidate.update_score(single_posting.doc_id, single_posting.term_freq);
+            let token = token.clone();
+            let candidates = Arc::clone(&candidates);
+            // spawn a thread for each token
+            let handle = thread::spawn(move || {
+                // get the first letter of the token to determine which file to read
+                if let Some(first_char) = token.chars().next() {
+                    // go to the file_skip list of the first character
+                    let skiplist = file_skip_list::FileSkip::read_skip_list(first_char);
+                    // get which BYTE range it is in between
+                    let offset_range =
+                        file_skip_list::FileSkip::find_skip_entry(&skiplist, token.to_string());
+                    let file_path = format!("inverted_index/merged/{}.txt", first_char);
+
+                    let mut candidate = Candidate::new(token.to_string());
+                    if let Ok(file) = File::open(&file_path) {
+                        // get the postings from the file and update the scorings of the candidates
+                        let postings = file_skip_list::get_postings_from_offset_range(
+                            &file,
+                            offset_range,
+                            &token,
+                        );
+                        // Update candidates with the postings data
+                        for single_posting in postings.postings {
+                            candidate.update_score(single_posting.doc_id, single_posting.term_freq);
+                        }
+                    } else {
+                        println!("Warning: Could not open index file for '{}'", first_char);
                     }
-                } else {
-                    println!("Warning: Could not open index file for '{}'", first_char);
+                    let mut candidates = candidates.lock().unwrap();
+                    candidates.push(candidate);
                 }
-            }
+            });
+            handles.push(handle);
         }
+        // after each thread is done, join them
+        for handle in handles {
+            handle.join().unwrap();
+        }
+        // unwrap the candidates from the mutex lock
+        let mut candidates = Arc::try_unwrap(candidates).unwrap().into_inner().unwrap();
         // sort the candidates by the number of documents they appear in with smallest first
         candidates.sort_by(|a, b| a.doc_ids.len().cmp(&b.doc_ids.len()));
         // then filter candidates that only have all the tokens
@@ -88,6 +111,7 @@ impl SearchEngine {
     }
 }
 
+#[derive(Debug)]
 pub struct Candidate {
     pub term: String,
     pub doc_ids: HashMap<u16, u16>, // for each doc_id, the term frequency
