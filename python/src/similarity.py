2e195464c63fed6b1fac356868bed91986e6551f
Christian Lasam <christianlasam1@gmail.com>
2025-03-07 04:00:00 -0800

    working on similarity methods

--- python/src/similarity.py
+++ python/src/similarity.py
@@ -6,4 +6,20 @@ class SimilarityDeterctor:
     """
     def __init__(self):
         self.seen_checksums = set()
-        self.page_simhashes = {}
+        self.page_simhashes = {}\
+        
+    def clean_text(self, text:str) -> str:
+        text = ' '.join(text.strip().split())
+        return text.lower()
+    
+    def calculuate_similarity(self, ngrams1: set, ngrams2: set) -> float:
+        if not ngrams1 or not ngrams2:
+            return 0.0
+        intersection = len(ngrams1 & ngrams2)
+        union = len(ngrams1|ngrams2)
+        return intersection / union if union > 0 else 0.0
+    
+    #not sure what we passin in to here
+    def is_duplicate_or_similar(self, path: str, text:str) -> tuple[bool, str]:
+        clean_text = self.clean_text(text)
+        return False, "unique"
