664d315f2619c4995dd622684015ff48bde33669
TallaJ <tallaj@uci.edu>
2025-03-08 21:10:45 -0800

    implement tf-idf, however need to update total documents

--- python/src/query.py
+++ python/src/query.py
@@ -3,11 +3,12 @@ import os
 from collections import defaultdict
 from typing import List
 from tokenizer import Tokenizer
+import math
 
-OUTPUT_DIR = "./merged_indexes"
+SRC_DIR = os.path.dirname(os.path.abspath(__file__))
+OUTPUT_DIR = os.path.join(SRC_DIR, "merged_indexes")
 
-# ! Do we have a function for this? idk
-
+TOTAL_DOCUMENT_COUNT = 10000  # Replace with the actual total document count
 
 def get_postings(token):
     """
@@ -28,6 +29,13 @@ def get_postings(token):
                     break  # stop after finding the token
     return postings
 
+def scoring_tf_idf(term_freq: int, posting_length: int) -> float:
+    """
+    Calculate the TF-IDF score for a term in a document.
+    """
+    tf = math.log2(term_freq) + 1.0
+    idf = math.log2(TOTAL_DOCUMENT_COUNT / posting_length)
+    return tf * idf
 
 class Candidate:
     """
@@ -38,11 +46,12 @@ class Candidate:
         self.doc_id = doc_id
         self.tokens_matched = {}
 
-    def update_score(self, token, frequency):
-        self.tokens_matched[token] = frequency
+    def update_score(self, token, score):
+        self.tokens_matched[token] = score
 
     def has_all_tokens(self, query_tokens):
         return all(token in self.tokens_matched for token in query_tokens)
+    
 
 
 class SearchEngine:
@@ -82,7 +91,8 @@ class SearchEngine:
             for doc_id, frequency in postings.items():
                 if doc_id not in candidates:
                     candidates[doc_id] = Candidate(doc_id)
-                candidates[doc_id].update_score(token, frequency)
+                score = scoring_tf_idf(frequency, len(postings))
+                candidates[doc_id].update_score(token, score)
         # Filter candidates to only those that match all query tokens
         valid_candidates = get_valid_candidates(candidates, self.tokens)
 
@@ -91,14 +101,16 @@ class SearchEngine:
             c.tokens_matched.values()), reverse=True)
         # Take only top 5 results if available
         valid_candidates = valid_candidates[:5]
+        finished_time = time.time() - start_time
         if valid_candidates:
             print("Matching Documents:")
             for i, candidate in enumerate(valid_candidates):
                 print(
                     f"{i+1}. Document ID: {candidate.doc_id}, Score: {candidate.tokens_matched[token]}")
         else:
-            return []        # Time
-        print(f"Search completed in {time.time() - start_time:.2f} seconds.")
+            return [] 
+        
+        print(f"Search completed in {finished_time} seconds.")
         return [c.doc_id for c in valid_candidates]
 
 
