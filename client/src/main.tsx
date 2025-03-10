7bd5827768830a4868452279cfe54025ccebf42c
charlieweinberger <charlieweinberger05@gmail.com>
2025-03-09 17:27:54 -0700

    got rid of old app.tsx, commented out AI code (for now)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import App from './App.tsx' // TODO: uncomment
-import App from './App2.tsx' // TODO: remove
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
